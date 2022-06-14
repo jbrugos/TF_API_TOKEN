2fe29c4e68f3b536e08c8910147a714b33a05019
jbrugos <106165746+jbrugos@users.noreply.github.com>
2022-06-14 19:13:32 +0100

    demo schema

--- main.tf
+++ main.tf
@@ -22,5 +22,9 @@ resource "snowflake_database" "demo_db_two" {
   name    = "DEMO_DB_TWO"
   comment = "Database for Snowflake Terraform demo"
 }
-
+resource "snowflake_schema" "demo_schema_two" {
+  database = snowflake_database.demo_db_two.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
 
